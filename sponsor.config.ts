8e95141ec1881fb78eb45d9f6d11811b38adf99e
Pugmatt <pugmatt@gmail.com>
2025-05-26 14:55:22 -0400

    Update sponsor.config.ts

--- sponsor.config.ts
+++ sponsor.config.ts
@@ -12,31 +12,6 @@ const past: BadgePreset = {
 }
 
 export default defineConfig({
-  onSponsorsFetched: function(sponsors, provider) {
-    for (let i = 0; i < 750; i++) {
-       sponsors.push({
-       sponsor: {
-          type: 'User',
-          login: "testlogin" + i,
-          name: "Test Name" + i,
-          avatarUrl: "https://picsum.photos/200"
-       },
-         monthlyDollars: Math.floor(Math.random()*17)
-      });
-    }
-    for (let i = 750; i < 760; i++) {
-       sponsors.push({
-       sponsor: {
-          type: 'User',
-          login: "testlogin" + i,
-          name: "Test Name" + i,
-          avatarUrl: "https://picsum.photos/200"
-       },
-         monthlyDollars: 50+Math.floor(Math.random()*8)
-      });
-    }
-  return sponsors;
-  },
   sponsorsAutoMerge: true,
   tiers: [
     {
